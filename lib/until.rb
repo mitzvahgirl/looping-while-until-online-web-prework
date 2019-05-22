a300747d059d206e4cc88cfd6b0cec2cf38f79e9
Amy Fogelman <abfnj1@gmail.com>
2019-05-22 01:46:29 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,5 @@
-while levitation_force<10
-    puts "Wingardium Leviosa"
-    levitation_force+=1
-	#your code here
+  until levitation_force==10
+      puts "Wingardium Leviosa"
+      levitation_force+=1
+    end
 end
-
